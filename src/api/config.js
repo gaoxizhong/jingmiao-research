1b94764ff87530d3612f7bb6b0874e4caab2bff5
gaoxizhong <296203386@qq.com>
2023-05-19 18:18:26 +0800

    优化收藏接口

--- src/api/config.js
+++ src/api/config.js
@@ -2,10 +2,10 @@ export default {
     title:'admin',
     baseURL: {
       
-        //主站 -- 测试环境
+        //森亿 -- 环境
         dev: 'http://172.16.70.33/jmjk/',
-        //主站 -- 正式环境
-        pro: 'https://jm.jiankangche.cn/jmjk/',
+        //主站 
+        pro: 'http://182.43.12.243:8981/jmjk/',
         
     }
 }
